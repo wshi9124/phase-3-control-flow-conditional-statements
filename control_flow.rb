4f5a0cfa5cebd527ecca855f8b8a4a5260c6a972
Willie Shi <wshi9124@gmail.com>
2022-07-17 23:29:59 -0400

    commit tree f007f19ed3bcd318f2584d157f70b2f739f267d8

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,51 @@
 def admin_login(username, password)
-  # your code here
+  if (username == "admin" || username =="ADMIN") && password == "12345"
+    "Access granted"
+  else
+    "Access denied"
+end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  response = ""
+  if temperature < 40
+    response = "brisk" 
+  elsif temperature >= 40 && temperature <= 65
+    response = "a little chilly"
+  elsif temperature > 85
+    response = "too dang hot"
+  else
+    response = "perfect"
+  end
+    "It's #{response} out there!"
 end
+
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    num
+end
 end
 
-def calculator(operation, num1, num2)
-  # your code here
-end
-
+def calculator (operation, num1, num2)
+  case operation
+  when "+"
+    num1 + num2
+  when "-"
+    num1 - num2
+  when "*"
+    num1 * num2
+  when "/"
+    num1 / num2
+  else 
+    puts "Invalid operation!"
+    nil
+  end
+  end
+  
